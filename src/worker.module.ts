bdef0120944a5413bbe1e8c23bd2f40e38014d10
gavin-shaw <gavin@gnarus.io>
2022-05-06 18:59:04 +0200

    fix: mongo config

--- src/worker.module.ts
+++ src/worker.module.ts
@@ -1,14 +1,19 @@
-import { EkConfigService, SdkModule } from '@earnkeeper/ekp-sdk-nestjs';
+import { SdkModule } from '@earnkeeper/ekp-sdk-nestjs';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BattlesModule } from './feature/battles/battles.module';
 import { CardsModule } from './feature/cards/cards.module';
 import { LeaderboardModule } from './feature/leaderboard/leaderboard.module';
 import { StatsModule } from './feature/stats/stats.module';
+import { config } from './schedule/config';
 
 export const MODULE_DEF = {
   imports: [
-    MongooseModule.forRootAsync({ useClass: EkConfigService }),
+    MongooseModule.forRoot(
+      config('MONGO_URI', {
+        default: 'mongodb://localhost:27017/splinterlands',
+      }),
+    ),
     BattlesModule,
     CardsModule,
     LeaderboardModule,
